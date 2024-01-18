5431908ca52c79aefce26d040eb0a227131a1fc4
akshit29k <mograakshit@gmail.com>
2024-01-18 21:08:32 +0530

    Password hashing using bcrypt js and sending jwt token to user for authentication

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,37 +2,31 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 
-//Email Validating Method
-const validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-};
+
 
 //Creating schema for user of ibook
 const UserSchema = new Schema({
-    Name:{
+    name:{
         type:String,
         required:true,
         minLength: [3, 'Length should be minimum 3']
     },
-    Email:{
+    email:{
         type:String,
         required:true,
-        unique: [true,'Email already existed'],
-        validate: [validateEmail, 'Please fill a valid email address']
+        unique: [true,'Email already existed']
     },
-    Mobile:{
+    mobile:{
         type:String,
         required:true,
-        unique:true,
+        unique:[true,'Number already existed'],
         minLength: [10, 'Invalid Number'],
         maxLength: [10, 'Invalid Number']
     },
-    Password:{
+    password:{
         type:String,
         required:true,
-        minLength: [5, 'Minimum 5 Characters'],
-        maxLength: [9, 'Not more then 9 Characters']
+        minLength: [5, 'Minimum 5 Characters']
     },
     Date:{
         type:Date,
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,90 @@
 const express = require('express');
 const route = express.Router();
 const User =  require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchUser');
+const {check,body,validationResult} = require('express-validator');
 
-//Creating user 
-route.post(("/"), async (req,res)=>{
+const secretKey = "monstermodel"; 
+
+
+//Route 1 : Creating user /api/auth/createuser - no auth required
+route.post(("/createuser"),[body('email',"Should be a valid email").isEmail()], async (req,res)=>{
     try {
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt);
         //Saving user
-        const user = User(req.body);
-       await user.save();
-        res.status(200).send(user);
+        const user = await User.create({
+            name:req.body.name,
+            email:req.body.email,
+            mobile:req.body.mobile,
+            password:secPass
+        })
+
+        //Data for Jwt token
+        const data = {
+            userData:{
+                ID : user.id
+            }
+        }
+        //creating jwt token
+        const authToken = await jwt.sign(data,secretKey);
+        return res.status(200).send(authToken);
       } catch (error) {
         //Handling the error
         if (error.name === "ValidationError") {
-          let errors = {};
-    
-          Object.keys(error.errors).forEach((key) => {
-            errors[key] = error.errors[key].message;
-          });
-    
-          return res.status(400).send(errors);
+          return res.status(400).send(error.message);
         }
         res.status(500).send(error.message);
       }
+})
+
+
+//Route 2 : Validating user /api/auth/login - no auth required
+route.post(("/login"),[body('email',"Should be a valid email").isEmail(),check('password',"Cannot be empty").not().isEmpty()],async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).send(errors);
+    }
+    try{
+        const {email,password} = req.body;
+        let user = await User.findOne({email});
+    
+        if(!user){
+            return res.status(400).json({error:"User does not exists"}); 
+        }     
+        const passwordCompare = await bcrypt.compare(password,user.password);
+
+        if(!passwordCompare){
+            return res.status(400).json({error:"Enter a correct password"});
+        }
+        //Data for Jwt token
+        const data = {
+            userData:{
+                ID : user.id
+            }
+        }
+        //creating jwt token
+        const authToken = await jwt.sign(data,secretKey);
+        res.status(200).send(authToken);
+
+    }catch(error){
+        console.error(error.message);
+        res.send(error);
+    }
+})
+
+
+//Route 3 : Get User /api/auth/getuser - Auth required
+route.post("/getuser",fetchuser, async (req,res)=>{
+    try{
+        const userId = req.user.ID;
+        const user =await User.findById(userId).select("-password");   
+        res.send(user);
+    }catch(error){
+        res.send(error);
+    }
 })
 
 module.exports = route
